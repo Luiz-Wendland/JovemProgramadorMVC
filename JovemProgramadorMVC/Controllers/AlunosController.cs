482b2028ba87a463222d269cca61943081198b76
bartg <bartg@DESKTOP-S9756AU>
2022-10-28 21:01:50 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorMVC/Controllers/AlunosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JovemProgramadorMVC.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
